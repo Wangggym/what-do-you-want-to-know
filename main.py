db6525a6d9b4558615495e42947d5bbe0a3726cb
wangym <wangym0203@gmail.com>
2023-10-06 21:02:03 +0800

    Query Parameters

--- main.py
+++ main.py
@@ -14,10 +14,25 @@ class ModelName(str, Enum):
 async def root():
     return {"message": "Hello World"}
 
+# Optional parameters
+# The same way, you can declare optional query parameters, by setting their default to None:
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"})
+    return item
 
-@app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 
 @app.get("/users")
@@ -34,3 +49,11 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, 'message': 'LeCNN all the images'}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+fake_items_db = [{'item_name': 'Foo'}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get('/items')
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip+limit]
