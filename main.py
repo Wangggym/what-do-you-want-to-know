c8e99e3350fd05c66a06122b235f53393bb7a4e4
wangym <wangym0203@gmail.com>
2023-10-07 08:53:41 +0800

    Request Body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,6 +17,8 @@ async def root():
 
 # Optional parameters
 # The same way, you can declare optional query parameters, by setting their default to None:
+
+
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id, "owner_id": user_id}
@@ -57,3 +60,18 @@ fake_items_db = [{'item_name': 'Foo'}, {
 @app.get('/items')
 async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip+limit]
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put('/items/{item_id}')
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
